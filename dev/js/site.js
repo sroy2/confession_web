3818323ecbc069a10980ec02cf211904e6b0e815
Stephen Roy <sroy2@hotmail.com>
2012-02-19 22:59:38 +0000

    updated menu js & site.js format

--- dev/js/site.js
+++ dev/js/site.js
@@ -1,30 +1,25 @@
 { "ip":"173.255.226.98",
   "name":"www.violentpython.org",
   "menu":[{
-      "header":"Registry",
-      "body":"Some random Registry text to be displayed."
+      "item":"Registry"
     },
     {
-      "header":"Documents",
-      "body":"Some random Document text to be displayed."
+      "item":"Documents"
     },
     {
-      "header":"Volatile",
-      "body":"Some random Volatile text to be displayed."
+      "item":"Volatile"
     },
     {
-      "header":"Browser",
-      "body":"Some random Browser text to be displayed.",
-      "sub_menu":[
-        {"name":"Chrome"},
-        {"name":"Firefox"},
-        {"name":"Internet Explorer"},
-        {"name":"Opera"},
-        {"name":"Safari"}]
+      "item":"Browser",
+      "menu":[
+        {"item":"Chrome"},
+        {"item":"Firefox"},
+        {"item":"Internet Explorer"},
+        {"item":"Opera"},
+        {"item":"Safari"}]
     },
     {
-      "header":"Applications",
-      "body":"Some random Applications text to be displayed."
+      "item":"Applications"
     }
   ]
 }
