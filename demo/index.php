deadd61fbba0b75aa006352eee6f2fd691c3a888
Stephen Roy <sroy2@hotmail.com>
2012-02-20 21:59:41 +0100

    removed extraneous files

--- demo/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-print "<html><head><title>index.html</title></head><body><xmp>";
-//include('index.html');
-$data = file_get_contents("index.html");
-print $data;
-print "</xmp></body></html>";
-?>
