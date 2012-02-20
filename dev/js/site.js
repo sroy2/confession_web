40234e964b4d8178337a84ed10a1bcfdbf7e1145
Stephen Roy <sroy2@hotmail.com>
2012-02-20 05:20:34 +0100

    updated site.js

--- dev/js/site.js
+++ dev/js/site.js
@@ -21,5 +21,51 @@
     {
       "item":"Applications"
     }
-  ]
+  ],
+  "Registry":[
+    {"reg1":"value1"},
+    {"reg2":"value2"},
+    {"windows_version":"7 SP1"}
+  ],
+  "Documents":[
+    {"user":[
+      {"foo.txt":"10JAN2010"},
+      {"bar.docx":"12FEB2012"},
+      {"p0rn.pptx":"01APR2013"}
+    ]},
+    {"downloads":[
+      {"putty.exe":"07MAR1998"},
+      {"bittorrent.msi":"24MAY2009"}
+    ]}
+  ],
+  "Volatile":[
+    {"ram":"no fucking clue"}
+  ],
+  "Browser":"something generic?",
+  "Chrome":[
+    {"History":"array not a single value"},
+    {"Downloads":"tired of making up shit"},
+    {"Searches":"time to copy paste"}
+  ],
+  "Firefox":[
+    {"History":"array not a single value"},
+    {"Downloads":"tired of making up shit"},
+    {"Searches":"time to copy paste"}
+  ],
+  "Internet Explorer":[
+    {"History":"array not a single value"},
+    {"Downloads":"tired of making up shit"},
+    {"Searches":"time to copy paste"}
+  ],
+  "Opera":[
+    {"History":"array not a single value"},
+    {"Downloads":"tired of making up shit"},
+    {"Searches":"time to copy paste"}
+  ],
+  "Safari":[
+    {"History":"array not a single value"},
+    {"Downloads":"tired of making up shit"},
+    {"Searches":"time to copy paste"}
+  ],
+  "Applications":"if any of these weren't here we wouldn't have a menu listing for it"
 }
