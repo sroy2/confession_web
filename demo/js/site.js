0015f8b0872b3bc515841e9c1138265e0345907b
Stephen Roy <sroy2@hotmail.com>
2012-02-20 21:36:49 +0100

    updated format for site.js

--- demo/js/site.js
+++ demo/js/site.js
@@ -1,16 +1,10 @@
 { "ip":"173.255.226.98",
   "name":"www.violentpython.org",
-  "menu":[{
-      "item":"Registry"
-    },
-    {
-      "item":"Documents"
-    },
-    {
-      "item":"Volatile"
-    },
-    {
-      "item":"Browser",
+  "menu":[
+    {"item":"Registry"},
+    {"item":"Documents"},
+    {"item":"Volatile"},
+    {"item":"Browser",
       "menu":[
         {"item":"Chrome"},
         {"item":"Firefox"},
@@ -18,54 +12,92 @@
         {"item":"Opera"},
         {"item":"Safari"}]
     },
-    {
-      "item":"Applications"
-    }
+    {"item":"Applications"}
   ],
   "Registry":[
     {"reg1":"value1"},
     {"reg2":"value2"},
     {"windows_version":"7 SP1"}
   ],
-  "Documents":[
-    {"user":[
-      {"foo.txt":"10JAN2010"},
-      {"bar.docx":"12FEB2012"},
-      {"p0rn.pptx":"01APR2013"}
-    ]},
-    {"downloads":[
-      {"putty.exe":"07MAR1998"},
-      {"bittorrent.msi":"24MAY2009"}
-    ]}
-  ],
-  "Volatile":[
-    {"ram":"no fucking clue"}
-  ],
-  "Browser":"something generic?",
-  "Chrome":[
-    {"History":"array not a single value"},
-    {"Downloads":"tired of making up shit"},
-    {"Searches":"time to copy paste"}
-  ],
-  "Firefox":[
-    {"History":"array not a single value"},
-    {"Downloads":"tired of making up shit"},
-    {"Searches":"time to copy paste"}
-  ],
-  "Internet Explorer":[
-    {"History":"array not a single value"},
-    {"Downloads":"tired of making up shit"},
-    {"Searches":"time to copy paste"}
-  ],
-  "Opera":[
-    {"History":"array not a single value"},
-    {"Downloads":"tired of making up shit"},
-    {"Searches":"time to copy paste"}
-  ],
-  "Safari":[
-    {"History":"array not a single value"},
-    {"Downloads":"tired of making up shit"},
-    {"Searches":"time to copy paste"}
-  ],
-  "Applications":"if any of these weren't here we wouldn't have a menu listing for it"
+  "Documents":{
+    "user":[
+      {"name":"foo.txt", "modified":"10JAN2010", "size":"xxx MB"},
+      {"name":"bar.docx", "modified":"12FEB2012", "size":"yyy MB"},
+      {"name":"p0rn.pptx", "modified":"01APR2013", "size":"zzz MB"}],
+    "downloads":[
+      {"name":"putty.exe", "modified":"07MAR1998", "size":"xxx MB"},
+      {"name":"bittorrent.msi", "modified":"24MAY2009", "size":"yyy MB"}]
+  },
+  "Volatile":{
+    "dlls":[
+      {"name":"blah", "parent":"explorer?"}],
+    "kernel modules":[
+      {"name":"guess we're in linux", "automatic":"yep"}],
+    "open files":[
+      {"name":"p0rn.pptx", "location":"downloads"}]
+  },
+  "Browser":{
+    "display":"This page probably won't display info, but it might give links to the browsers...",
+    "links":[
+      {"name":"Chrome"},
+      {"name":"Firefox"},
+      {"name":"Internet Explorer"},
+      {"name":"Opera"},
+      {"name":"Safari"}]
+  },
+  "Chrome":{
+    "History":[
+      {"site":"url", "date":"date", "other_info":"blah"},
+      {"site":"another url", "date":"antoher date", "other_info":"blah"}],
+    "Downloads":[
+      {"site":"tired of making up shit", "date":"yesterday"}],
+    "Searches":[
+      {"term":"time to copy paste", "date":"guess what"},
+      {"term":"robert's mom", "date":"9 months before robert was born"}]
+  },
+  "Firefox":{
+    "History":[
+      {"site":"url", "date":"date", "other_info":"blah"},
+      {"site":"another url", "date":"antoher date", "other_info":"blah"}],
+    "Downloads":[
+      {"site":"tired of making up shit", "date":"yesterday"}],
+    "Searches":[
+      {"term":"time to copy paste", "date":"guess what"},
+      {"term":"robert's mom", "date":"9 months before robert was born"}]
+  },
+  "Internet Explorer":{
+    "History":[
+      {"site":"url", "date":"date", "other_info":"blah"},
+      {"site":"another url", "date":"antoher date", "other_info":"blah"}],
+    "Downloads":[
+      {"site":"tired of making up shit", "date":"yesterday"}],
+    "Searches":[
+      {"term":"time to copy paste", "date":"guess what"},
+      {"term":"robert's mom", "date":"9 months before robert was born"}]
+  },
+  "Opera":{
+    "History":[
+      {"site":"url", "date":"date", "other_info":"blah"},
+      {"site":"another url", "date":"antoher date", "other_info":"blah"}],
+    "Downloads":[
+      {"site":"tired of making up shit", "date":"yesterday"}],
+    "Searches":[
+      {"term":"time to copy paste", "date":"guess what"},
+      {"term":"robert's mom", "date":"9 months before robert was born"}]
+  },
+  "Safari":{
+    "History":[
+      {"site":"url", "date":"date", "other_info":"blah"},
+      {"site":"another url", "date":"antoher date", "other_info":"blah"}],
+    "Downloads":[
+      {"site":"tired of making up shit", "date":"yesterday"}],
+    "Searches":[
+      {"term":"time to copy paste", "date":"guess what"},
+      {"term":"robert's mom", "date":"9 months before robert was born"}]
+  },
+  "Applications":{
+    "info":"if any of these weren't here we wouldn't have a menu listing for it",
+    "installed":[
+      {"name":"CCleaner", "location":"~/Applications/CCleaner", "size":"20 MB", "version":"13.1"}]
+  }
 }
