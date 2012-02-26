a6fc8ffedccb9efd964d84eec37f6aa89f294960
Stephen Roy <sroy2@hotmail.com>
2012-02-27 00:58:12 +0100

    making tables, css work done

--- demo/js/site.js
+++ demo/js/site.js
@@ -1,18 +1,16 @@
 { "ip":"173.255.226.98",
   "name":"www.violentpython.org",
   "menu":[
-    {"item":"Registry"},
-    {"item":"Documents"},
-    {"item":"Volatile"},
-    {"item":"Browser",
-      "menu":[
-        {"item":"Chrome"},
-        {"item":"Firefox"},
-        {"item":"Internet Explorer"},
-        {"item":"Opera"},
-        {"item":"Safari"}]
-    },
-    {"item":"Applications"}
+    "Registry",
+    "Documents",
+    "Volatile",
+    {"Browser":[
+      "Chrome",
+      "Firefox",
+      "Internet Explorer",
+      "Opera",
+      "Safari"]},
+    "Applications"
   ],
   "Registry":[
     {"reg1":"value1"},
@@ -37,13 +35,15 @@
       {"name":"p0rn.pptx", "location":"downloads"}]
   },
   "Browser":{
-    "display":"This page probably won't display info, but it might give links to the browsers...",
-    "links":[
-      {"name":"Chrome"},
-      {"name":"Firefox"},
-      {"name":"Internet Explorer"},
-      {"name":"Opera"},
-      {"name":"Safari"}]
+    "info":"Text to display...",
+    "headings":[
+      "Browser"],
+    "rows":[
+      ["<a href='./#Chrome'>Chrome</a>"],
+      ["<a href='./#Firefox'>Firefox></a>"],
+      ["<a href='./#Internet Explorer'>Internet Explorer</a>"],
+      ["<a href='./#Opera'>Opera</a>"],
+      ["<a href='./#Safari'>Safari</a>"]]
   },
   "Chrome":{
     "History":[
@@ -96,8 +96,73 @@
       {"term":"robert's mom", "date":"9 months before robert was born"}]
   },
   "Applications":{
-    "info":"if any of these weren't here we wouldn't have a menu listing for it",
-    "installed":[
-      {"name":"CCleaner", "location":"~/Applications/CCleaner", "size":"20 MB", "version":"13.1"}]
+    "info":"Text to display...",
+    "headings":[
+      "Name", "Location", "Version"],
+    "rows":[
+      ["CCleaner", "~/Applications/CCleaner", "13.1"],
+      ["AppDelete", "~/Applications/AppDelete", "1.0"]]
+  },
+  "demo":{
+    "info":"Text to display...",
+    "headings":[
+      "Rendering engine", "Browser", "Platform(s)", "Engine version", "CSS grade"],
+    "rows":[
+      ["Trident", "Internet Explorer 4.0", "Win 95+", "4", "X"],
+      ["Trident", "Internet Explorer 5.0", "Win 95+", "5", "C"],
+      ["Trident", "Internet Explorer 5.5", "Win 95+", "5.5", "A"],
+      ["Trident", "Internet Explorer 6", "Win 98+", "6", "A"],
+      ["Trident", "Internet Explorer 7", "Win XP SP2+", "7", "A"],
+      ["Gecko", "Firefox 1.0", "Win 98+ / OSX.2+", "1.7", "A"],
+      ["Gecko", "Firefox 1.5", "Win 98+ / OSX.2+", "1.8", "A"],
+      ["Gecko", "Firefox 2.0", "Win 98+ / OSX.2+", "1.8", "A"],
+      ["Gecko", "Firefox 3.0", "Win 2k+ / OSX.3+", "1.9", "A"],
+      ["Gecko", "Camino 1.0", "OSX.2+", "1.8", "A"],
+      ["Gecko", "Camino 1.5", "OSX.3+", "1.8", "A"],
+      ["Gecko", "Netscape 7.2", "Win 95+ / Mac OS 8.6-9.2", "1.7", "A"],
+      ["Gecko", "Netscape Browser 8", "Win 98SE+", "1.7", "A"],
+      ["Gecko", "Netscape Navigator 9", "Win 98+ / OSX.2+", "1.8", "A"],
+      ["Gecko", "Mozilla 1.0", "Win 95+ / OSX.1+", "1", "A"],
+      ["Gecko", "Mozilla 1.1", "Win 95+ / OSX.1+", "1.1", "A"],
+      ["Gecko", "Mozilla 1.2", "Win 95+ / OSX.1+", "1.2", "A"],
+      ["Gecko", "Mozilla 1.3", "Win 95+ / OSX.1+", "1.3", "A"],
+      ["Gecko", "Mozilla 1.4", "Win 95+ / OSX.1+", "1.4", "A"],
+      ["Gecko", "Mozilla 1.5", "Win 95+ / OSX.1+", "1.5", "A"],
+      ["Gecko", "Mozilla 1.6", "Win 95+ / OSX.1+", "1.6", "A"],
+      ["Gecko", "Mozilla 1.7", "Win 98+ / OSX.1+", "1.7", "A"],
+      ["Gecko", "Mozilla 1.8", "Win 98+ / OSX.1+", "1.8", "A"],
+      ["Gecko", "Seamonkey 1.1", "Win 98+ / OSX.1+", "1.8", "A"],
+      ["Gecko", "Epiphany 2.20", "Gnome", "1.8", "A"],
+      ["Webkit", "Safari 1.2", "OSX.3", "125.5", "A"],
+      ["Webkit", "Safari 1.3", "OSX.3", "312.8", "A"],
+      ["Webkit", "Safari 2.0", "OSX.4+", "419.3", "A"],
+      ["Webkit", "Safari 3.0", "OSX.4+", "522.1", "A"],
+      ["Webkit", "OmniWeb 5.5", "OSX.4+", "420", "A"],
+      ["Webkit", "iPod Touch / iPhone", "iPod", "420.1", "A"],
+      ["Webkit", "S50", "S60", "413", "A"],
+      ["Presto", "Opera 7.0", "Win 95+ / OSX.1+", "-", "A"],
+      ["Presto", "Opera 7.5", "Win 95+ / OSX.2+", "-", "A"],
+      ["Presto", "Opera 8.0", "Win 95+ / OSX.2+", "-", "A"],
+      ["Presto", "Opera 8.5", "Win 95+ / OSX.2+", "-", "A"],
+      ["Presto", "Opera 9.0", "Win 95+ / OSX.3+", "-", "A"],
+      ["Presto", "Opera 9.2", "Win 88+ / OSX.3+", "-", "A"],
+      ["Presto", "Opera 9.5", "Win 88+ / OSX.3+", "-", "A"],
+      ["Presto", "Opera for Wii", "Wii", "-", "A"],
+      ["Presto", "Nokia N800", "N800", "-", "A"],
+      ["Presto", "Nintendo DS browser", "Nintendo DS", "8.5", "C/A"],
+      ["KHTML", "Konqureror 3.1", "KDE 3.1", "3.1", "C"],
+      ["KHTML", "Konqureror 3.3", "KDE 3.3", "3.3", "A"],
+      ["KHTML", "Konqureror 3.5", "KDE 3.5", "3.5", "A"],
+      ["Tasman", "Internet Explorer 4.5", "Mac OS 8-9", "-", "X"],
+      ["Tasman", "Internet Explorer 5.1", "Mac OS 7.6-9", "1", "C"],
+      ["Tasman", "Internet Explorer 5.2", "Mac OS 8-X", "1", "C"],
+      ["Misc", "NetFront 3.1", "Embedded devices", "-", "C"],
+      ["Misc", "NetFront 3.4", "Embedded devices", "-", "A"],
+      ["Misc", "Dillo 0.8", "Embedded devices", "-", "X"],
+      ["Misc", "Links", "Text only", "-", "X"],
+      ["Misc", "Lynx", "Text only", "-", "X"],
+      ["Misc", "IE Mobile", "Windows Mobile 6", "-", "C"],
+      ["Misc", "PSP browser", "PSP", "-", "C"],
+      ["Misc", "Other browsers", "All others", "-", "U"]]
   }
 }
